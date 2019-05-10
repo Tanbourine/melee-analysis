ac8a7be5e717ff923e4f7052ff02027c3bcaeab0
Darren Tan <darren.tan@spacex.com>
2019-05-10 01:36:13 -0700

    find rebirths

--- melee_analysis.py
+++ melee_analysis.py
@@ -4,6 +4,7 @@
 
 
 from slippi import Game
+from slippi.id import ActionState
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,29 +33,48 @@ class MeleeAnalysis():
             p0_damage.append(self.ports[0][i].damage)
             p1_damage.append(self.ports[1][i].damage)
 
-        ax.plot(p0_damage)
-        ax.plot(p1_damage)
+        ax.plot(p0_damage, label='Player 1')
+        ax.plot(p1_damage, label='Player 2')
+        ax.legend(loc='best')
         plt.show()
 
     def slice_state(self, port, start, end):
         data = []
         for i in range(start, end):
-            data.append(self.ports[port][i].state)
+            frame = self.ports[port][i]
+            data.append((i, frame.damage, frame.stocks, frame.state))
         return data
+
+    def find_rebirths(self):
+        rebirths = []
+        for i, port in enumerate(self.ports):
+            for j, frame in enumerate(port):
+                # if frame.state == ActionState.REBIRTH:
+                if frame.state == ActionState.REBIRTH:
+                    rebirths.append((i, j))
+
+        # filter out frames from the same rebirth
+        slim_rebirths = []
+        for n in range(0, len(rebirths), 60):
+            slim_rebirths.append(rebirths[n])
+
+        return slim_rebirths
+
+
 
 
 def main():
     file = 'replays/newgame.slp'
     game = MeleeAnalysis(file)
 
-    game.plot_damage()
 
-    # for i in range(1800, 1870):
-    #     print(game.ports[1][i].state)
+    rebirths = game.find_rebirths()
+    print(rebirths)
 
-    # slice = game.slice_state(1, 1830, 1870)
+    # slice = game.slice_state(0, 4250, 4350)
     # print(slice)
 
+    # game.plot_damage()
 
 if __name__ == '__main__':
     main()
